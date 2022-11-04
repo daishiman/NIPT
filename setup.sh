7c9ed79e93664b44c62f0b05cb4decfa0113ec79
AzureAD\higuchiyosuke <higuchi.yosuke@medical-frontier.jp>
2022-11-04 17:54:59 +0900

    ・ gitsubmodules の追加 ・ Laravel APP env漏れの修正 ・ setup.sh のカスタマイズ (現段階)

--- setup.sh
+++ setup.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 # windows環境、エラー吐かれる場合は git config --global core.autocrlf input を設定した後、再度 git clone を実行
 # ref. https://prograshi.com/platform/docker/convert-crlf-to-lf/
-PROJECT_NAME='bc_krt'
 PROJECT_DIR="/var/www/html"
 echo "cd $PROJECT_DIR && composer install"
-docker-compose exec web sh -c "cd $PROJECT_DIR && composer install"
-docker-compose exec web sh -c "chmod 777 -R $PROJECT_DIR/bootstrap/cache"
-docker-compose exec web sh -c "chmod 777 -R $PROJECT_DIR/storage"
-docker-compose exec web sh -c "cd $PROJECT_DIR && php artisan migrate"
-docker-compose exec web sh -c "cd $PROJECT_DIR && php artisan db:seed"
-docker-compose exec web sh -c "chmod 777 -R $PROJECT_DIR/storage"
-docker-compose exec web sh -c "cd $PROJECT_DIR && npm install"
-docker-compose exec web sh -c "cd $PROJECT_DIR && npm run build"
+
+# 患者様側
+docker-compose exec webapp sh -c "cd $PROJECT_DIR && composer install"
+docker-compose exec webapp sh -c "chmod 777 -R $PROJECT_DIR/bootstrap/cache"
+docker-compose exec webapp sh -c "chmod 777 -R $PROJECT_DIR/storage"
+docker-compose exec webapp sh -c "cd $PROJECT_DIR && npm install"
+docker-compose exec webapp sh -c "cd $PROJECT_DIR && npm run build"
+
+# 管理画面側
+docker-compose exec adminapp sh -c "cd $PROJECT_DIR && composer install"
+docker-compose exec adminapp sh -c "chmod 777 -R $PROJECT_DIR/bootstrap/cache"
+docker-compose exec adminapp sh -c "chmod 777 -R $PROJECT_DIR/storage"
+# migrate seederできたら
+# docker-compose exec adminapp sh -c "cd $PROJECT_DIR && php artisan migrate"
+# docker-compose exec adminapp sh -c "cd $PROJECT_DIR && php artisan db:seed"
+docker-compose exec adminapp sh -c "cd $PROJECT_DIR && npm install"
+docker-compose exec adminapp sh -c "cd $PROJECT_DIR && npm run build"
